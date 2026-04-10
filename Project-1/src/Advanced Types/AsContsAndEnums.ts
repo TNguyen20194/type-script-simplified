538181ecbd4f8ccaeb21d239f65cca0df7e894ad
Truong Nguyen <truong.nguyen201@gmail.com>
2026-04-10 14:10:26 -0400

    Update file

---
+++ Project-1/src/Advanced Types/AsContsAndEnums.ts
@@ -0,0 +1,20 @@
+const SKILL_LEVELS = ["Beginner", "Intermediate", "Expert"] as const
+
+type Person = {
+    skillLevel: (typeof SKILL_LEVELS)[number]
+};
+
+
+SKILL_LEVELS.forEach(skillLevel => {
+    console.log(skillLevel)
+})
+
+const person = {
+    name: "TN",
+    age: 33,
+    address: {
+        street: "Main St"
+    }
+} as const 
+
+person.age = 2
---
+++ Project-1/src/Advanced Types/Tuples.ts
@@ -0,0 +1,7 @@
+const person = {
+    name: "TN",
+    age: 32
+}
+
+type Tuple = [string, boolean];
+const a: Tuple = ["string", true]
